7ce09def02fe6313ad0440a8659caee8818368d5
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2020-01-11 17:47:36 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        String s = sc.next();
+        System.out.println(System.getProperty("java.version"));
+    }
+}
